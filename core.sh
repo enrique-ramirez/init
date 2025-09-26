a5bb269ed8f45b683b2c75497b0d0444c602f12b
Enrique Ramírez <hello@enriqueramirez.com>
2025-09-26 14:35:38 -0600

    feat: Split variables into separate file

--- core.sh
+++ core.sh
@@ -1,14 +1,4 @@
-# CONFIG
-ZSHRC_FILE="$HOME/.zshrc"
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-NC='\033[0m'
+source vars.sh
 
 echo "\n--------------------------------"
 echo "\n${YELLOW}Software installs!${NC}\n"
--- ssh-key.sh
+++ ssh-key.sh
@@ -1,21 +1,11 @@
-SSH_CONFIG_FILE="${HOME}/.ssh/config"
-SSH_ID_FILE_PATH="${HOME}/.ssh/id_ed25519"
+source vars.sh
+
 SSH_CONFIG_FILE_CONTENTS="
 Host github.com
   HostName github.com
   User git
   IdentityFile ${SSH_ID_FILE_PATH}
 "
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-WHITE='\033[0;37m'
-NC='\033[0m'
-
 echo "\n${YELLOW}Generating SSH key...${NC}"
 # ssh-keygen -t rsa -b 4096 -C "hello@enrique-ramirez.com"
 # eval "$(ssh-agent -s)"
